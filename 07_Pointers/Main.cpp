dcdaa0c0a376d5772b39ca473072cc3adfe1cba7
DriscollEthan <driscollethan@hotmail.com>
2025-08-25 10:52:36 -0700

    Pointers are interesting things that allow me to be unsafe with memory, if the compilers allows you to, esepcially dynamically.

--- 07_Pointers/Main.cpp
+++ 07_Pointers/Main.cpp
@@ -1,8 +1,38 @@
+#define NEWLINE (std::cout << std::endl)
+
 #include <iostream>
 
+struct Vector2D
+{
+  float x, y;
+};
 
+void ZeroVector2D(Vector2D* Vector2DRef)
+{
+  Vector2DRef->x = 0;
+  Vector2DRef->y = 0;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+  int* x = new int;
+  *x = 10;
+
+  int Size = 3;
+  Size += 5;
+  int* xArray = new int[Size];
+
+  std::cout << *x;
+
+  NEWLINE;
+
+  for (int i = 0; i < Size; ++i)
+  {
+    xArray[i] = i + 11;
+    std::cout << xArray[i];
+    NEWLINE;
+  }
+
+  delete x;
+  delete[] xArray;
 }
