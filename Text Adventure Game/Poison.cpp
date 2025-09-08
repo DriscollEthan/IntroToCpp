181cab5dae223f4e4a15856befcd6cb9df441bcc
DriscollEthan <driscollethan@hotmail.com>
2025-09-08 12:42:48 -0700

    Changes numbers on Poison for Balance.

--- Text Adventure Game/Poison.cpp
+++ Text Adventure Game/Poison.cpp
@@ -5,7 +5,7 @@ Poison::Poison()
 	SpellName = "Poison";
 	SpellDescription = "Poison is a spell that does some damage, and will deal damage to an enemy over time.";
 	Type = E_Poison;
-	Damage = 10.0f;
+	Damage = 5.0f;
 	DamageOverTime = 15.0f;
 	bStuns = false;
 	RechargeLength = 3;
